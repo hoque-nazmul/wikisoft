af6244db9a958b95d7736c1050a168a7f26a3081
nazmul98 <coder.nazmul@gmail.com>
2021-07-23 02:16:37 -0400

    feat: added tag archive

---
+++ wp-content/themes/wikisoft/tag.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<!-- Latest Post Area -->
+<section class="s-content">
+    <div class="row narrow">
+        <div class="col-full s-content__header" data-aos="fade-up">
+            <h1>
+                <?php 
+                    _e('Tag: ', 'wikisoft');
+                    single_tag_title(); 
+                ?>
+            </h1>
+
+            <p class="lead">
+                <?php echo tag_description(); ?>
+            </p>
+        </div>
+    </div>
+    
+    <div class="row masonry-wrap">
+        <div class="masonry">
+            <div class="grid-sizer"></div>
+
+            <?php 
+                if (!have_posts()) {
+                    _e("<h2 class='text-center'>No Post Found</h2>", 'wikisoft');
+                } else {
+                    while(have_posts()) {
+                        the_post();
+                        get_template_part('template-parts/post-formats/content', get_post_format());
+                    }
+                }
+            ?>
+        </div> 
+    </div> 
+    <?php get_template_part('template-parts/common/pagination'); ?>
+</section> 
+<!-- End Latest Post Area -->
+<?php get_footer(); ?>

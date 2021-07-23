13d1c0d6c8e2117e45c72cb037177d702d16a4ba
nazmul98 <coder.nazmul@gmail.com>
2021-07-22 22:05:17 -0400

    feat: comment system integrated & styled

---
+++ wp-content/themes/wikisoft/comments.php
@@ -0,0 +1,49 @@
+
+<div class="comments-wrap">
+
+    <div id="comments" class="row">
+        <div class="col-full">
+
+            <h3 class="h2">
+                <?php 
+                    $comments_num = get_comments_number();
+                    if ($comments_num <= 1) {
+                        _e($comments_num. ' Comment', 'wikisoft');
+                    } else {
+                        _e($comments_num. ' Comments', 'wikisoft');
+                    }
+                ?>
+            </h3>
+
+            <!-- commentlist -->
+            <ol class="commentlist">
+                <?php wp_list_comments(); ?>
+            </ol> <!-- end commentlist -->
+
+            <div class="comment-pagination">
+                <?php 
+                    get_the_comments_pagination([
+                        'screen_reader_text' => __(' ', 'wikisoft'),
+                        'prev_text' => __('Prev Comments', 'wikisoft'),
+                        'next_text' => __('Next Comments', 'wikisoft')
+                    ])
+                ?>
+            </div>
+
+            <!-- respond
+            ================================================== -->
+            <div class="respond">
+
+                <?php 
+                    comment_form([
+                        'label_submit' => __('Submit', 'wikisoft'),
+                        'title_reply' => __('Add Comment', 'wikisoft')
+                    ]);
+                ?>
+
+            </div> 
+
+        </div> 
+
+    </div>
+</div>
